d428d8e91d6c927b16e31776e4de0e2a98df6dee
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-07-08 19:16:18 -0700

    components altered

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,38 +1,105 @@
 import React, { Component } from "react";
 import { Container, Row, Column } from "../../components/Grid";
-import Article from "../../components/Article";
-import Calculator from "../../components/Calculator";
+import Article from "../../components/Card";
 // import Goals from "../../components/Goals";
 // import Graph from "../../components/Graph";
 import Navbar from "../../components/Navbar";
 // import Money from "../../components/Money";
 // import Login from "../../components/Login";
 // import Register from "../../components/Register";
+import Jumbotron from "../../components/Jumbotron";
+import Result from "../../components/Result";
+import KeyPad from "../../components/KeyPad";
 // import API from "../../utils/API";
 
 class Dashboard extends Component {
-    state = { 
-        toLogin: false,
-        toRegister: false,
-        login: [],
-        register: [],
+    constructor() {
+        super();
+
+        this.state = {
+            result: "",
     };
+}
+
+    onClick = (button) => {
+        if (button === "=") {
+            this.calculate();
+        } else if (button === "C") {
+            this.reset();
+        } else if (button === "CE") {
+            this.backspace();
+        } else {
+            this.setState({
+            result: this.state.result + button,
+    });
+  }
+};
+
+    calculate = () => {
+        var checkResult = "";
+        if (this.state.result.includes("--")) {
+            checkResult = this.state.result.replace("--", "+");
+        } else {
+            checkResult = this.state.result;
+        }
+
+        try {
+            this.setState({
+            // eslint-disable-next-line
+                result: (eval(checkResult) || "") + "",
+        });
+        } catch (e) {
+            this.setState({
+                result: "error",
+            });
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: "",
+        });
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1),
+        });
+    };
+
+   
+    // state = { 
+    //     toLogin: false,
+    //     toRegister: false,
+    //     login: [],
+    //     register: [],
+    // };
 
     render() {
         return (
           <div>
             <Navbar />
             <Container>
-              <Row>
-                <Column>
-                <Article />
-                </Column>
-              </Row>
+            <div>
+                <Jumbotron fluid>
+                    <h1>Money Tok</h1>
+                </Jumbotron>
+
+                <div className="calculator-body">
+                    <Result result={this.state.result} />
+                    <KeyPad onClick={this.onClick} />
+                </div>
+                <Container>
+                  <Row>
+            <Column>
+            <Article />
+            </Column>
+          </Row>
+        </Container>
+            </div>
             </Container>
-            <Container>
-                <Calculator />
-            </Container>
-          </div>
+         
+        </div>
         );
       }
 }
