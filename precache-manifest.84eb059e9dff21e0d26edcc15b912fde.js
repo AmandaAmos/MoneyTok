cbbf21ad58596f9910ddbd20144a85d6714bc96c
Amanda Amos <59996872+AmandaAmos@users.noreply.github.com>
2020-07-08 21:12:35 -0700

    Updates

---
+++ precache-manifest.84eb059e9dff21e0d26edcc15b912fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978cbd59fd60622666f7513342bbe636",
+    "url": "/MoneyTok/index.html"
+  },
+  {
+    "revision": "f0cc3e0ce0f1faaad31d",
+    "url": "/MoneyTok/static/css/main.1fa6cec3.chunk.css"
+  },
+  {
+    "revision": "d553e463fc033416f4c6",
+    "url": "/MoneyTok/static/js/2.48c133ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MoneyTok/static/js/2.48c133ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cc3e0ce0f1faaad31d",
+    "url": "/MoneyTok/static/js/main.af972702.chunk.js"
+  },
+  {
+    "revision": "7120cadc5ff468e32f67",
+    "url": "/MoneyTok/static/js/runtime-main.7c878f69.js"
+  }
+]);
