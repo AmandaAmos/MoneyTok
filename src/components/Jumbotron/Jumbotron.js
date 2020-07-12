92d97fbeddb51596fc8c247ce88dfd44575bb907
Ugni Blanc <ajo2185@gmail.com>
2020-07-12 10:43:56 -0700

    added income and expense cards, updated jumbrotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Jumbotron = ({ children }) => (
   <div className="jumbotron jumbotron-fluid">
-    <div 
-      style={{ height: 150, clear: "both", paddingTop: 10, paddingBottom: 20, textAlign: "Center"}}
-      className="container">
+    <div
+      style={{
+        height: 50,
+        clear: "both",
+        paddingTop: 30,
+        paddingBottom: 20,
+        textAlign: "Center",
+      }}
+      className="container"
+    >
       {children}
     </div>
   </div>
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { Container, Row, Column } from "../../components/Grid";
+import { Container } from "../../components/Grid";
 import { Card } from "react-bootstrap";
 import { CardColumns } from "react-bootstrap";
 import Navbar from "../../components/Navbar";
 import Jumbotron from "../../components/Jumbotron";
 import Result from "../../components/Result";
 import KeyPad from "../../components/KeyPad";
+import MoneyInTable from "../../components/MoneyInTable";
+import MoneyOutTable from "../../components/MoneyOutTable";
+import Button from "react-bootstrap/Button";
 // import API from "../../utils/API";
 
 class Dashboard extends Component {
@@ -17,6 +20,7 @@ class Dashboard extends Component {
     };
   }
 
+  //calculator function
   onClick = (button) => {
     if (button === "=") {
       this.calculate();
@@ -62,6 +66,7 @@ class Dashboard extends Component {
       result: this.state.result.slice(0, -1),
     });
   };
+  //end calculator function
 
   // state = {
   //     toLogin: false,
@@ -70,113 +75,99 @@ class Dashboard extends Component {
   //     register: [],
   // };
 
+  //page render
   render() {
     return (
       <div>
         <Navbar />
-        <Container>
-          <div>
-            <Jumbotron fluid>
-              <h1>Money Tok</h1>
-            </Jumbotron>
-            <Container>
-              <Row>
-                <Column>
-                  <Card />
-                </Column>
-                <Column>
-                  <Card />
-                </Column>
-                <Column>
-                  <Card />
-                </Column>
-              </Row>
+        <div>
+          <Jumbotron fluid>
+            <h1>Money Tok</h1>
+          </Jumbotron>
+          <Container>
+            <CardColumns>
 
-              {/* Anne testing code below */}
+              {/* Income Card */}
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Income</Card.Title>
+                  <Card.Title>(You Got Money!)</Card.Title>
+                  <MoneyInTable />
+                </Card.Body>
+                <Button variant="secondary" size="lg" block id="addIncome">
+                  Add New Income
+                </Button>
+              </Card>
 
-              <CardColumns>
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px160" />
-                  <Card.Body>
-                    <Card.Title>Card title that wraps to a new line</Card.Title>
-                    <Card.Text>
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-                <Card className="p-3">
-                  <blockquote className="blockquote mb-0 card-body">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Integer posuere erat a ante.
-                    </p>
-                    <footer className="blockquote-footer">
-                      <small className="text-muted">
-                        Someone famous in{" "}
-                        <cite title="Source Title">Source Title</cite>
-                      </small>
-                    </footer>
-                  </blockquote>
-                </Card>
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px160" />
-                  <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                      This card has supporting text below as a natural lead-in
-                      to additional content.{" "}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
+              <Card>
+                <Card.Body>
+                  <Card.Title> 2. Card title</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to
+                    additional content.{" "}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+
+
+                {/* Expense Card */}
+                <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Expense</Card.Title>
+                  <Card.Title>(You Spent Money!)</Card.Title>
+                  <MoneyOutTable />
+                </Card.Body>
+                <Button variant="secondary" size="lg" block id="addExpense">
+                  Add New Expense
+                </Button>
+              </Card>
+
+              <Card bg="primary" text="white" className="text-center p-3">
+                <blockquote className="blockquote mb-0 card-body">
+                  <p>
+                    3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer posuere erat a ante.
+                  </p>
+                  <footer className="blockquote-footer">
+                    <small className="text-muted">
+                      Someone famous in{" "}
+                      <cite title="Source Title">Source Title</cite>
+                    </small>
+                  </footer>
+                </blockquote>
+              </Card>
+
+
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>4. Card title</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to
+                    additional content.{" "}
+                  </Card.Text>
+                  <Card.Text>
                     <small className="text-muted">
                       Last updated 3 mins ago
                     </small>
-                  </Card.Footer>
-                </Card>
-                <Card bg="primary" text="white" className="text-center p-3">
-                  <blockquote className="blockquote mb-0 card-body">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Integer posuere erat a ante.
-                    </p>
-                    <footer className="blockquote-footer">
-                      <small className="text-muted">
-                        Someone famous in{" "}
-                        <cite title="Source Title">Source Title</cite>
-                      </small>
-                    </footer>
-                  </blockquote>
-                </Card>
-                <Card className="text-center">
-                  <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                      This card has supporting text below as a natural lead-in
-                      to additional content.{" "}
-                    </Card.Text>
-                    <Card.Text>
-                      <small className="text-muted">
-                        Last updated 3 mins ago
-                      </small>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
 
-                <Card>
-                  <Card.Body>
-                    <Card.Title>Calculator</Card.Title>
-                    <div className="calculator-body">
-                      <Result result={this.state.result} />
-                      <KeyPad onClick={this.onClick} />
-                    </div>
-                  </Card.Body>
-                </Card>
-              </CardColumns>
-            </Container>
-          </div>
-        </Container>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Calculator</Card.Title>
+                  <div className="calculator-body">
+                    <Result result={this.state.result} />
+                    <KeyPad onClick={this.onClick} />
+                  </div>
+                </Card.Body>
+              </Card>
+            </CardColumns>
+          </Container>
+        </div>
       </div>
     );
   }
